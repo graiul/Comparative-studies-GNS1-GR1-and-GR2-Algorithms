4be43ad634392c4b138cdcf4487ad8b913957ca8
DESKTOP-FOTVVAB\Gheorghica Radu <radugheorghe715@gmail.com>
2019-01-30 00:08:20 +0200

    Inserare label-uri pt noduri astfel incat sa fie percepute ca si label-uri de catre neo4j. Descoperit si sintaxa de adaugare al unui arc intre doua noduri, dar e o mica eroare de rezolvat pe parte de py.

--- neo4j_test.py
+++ neo4j_test.py
@@ -27,4 +27,15 @@ class neo4j_test(object):
     @staticmethod
     def create_neo4j_graph_node(tx, graph):
         for node in list(graph.nodes(data=True)):
-            tx.run("CREATE (a:Node {id: $id, label: $label})", id=node[0], label=list(list(node[1].values())))
+            # tx.run("CREATE (a:Node {id: $id, label: $label})", id=node[0], label=list(list(node[1].values())))
+            # print(str(list(list(node[1].values()))[0]))
+            cqlQuery = "CREATE (a:`" + str(list(list(node[1].values()))[0]) + "` {id: " + str(node[0]) + "})"
+            tx.run(cqlQuery)
+
+    @staticmethod
+    def create_neo4j_graph_edges(tx, graph):
+        for edge in graph.edges():
+            cqlQuery = "MATCH(a:`" + str(graph.node[edge[0]]['label']) + "`), (b:`" + str(graph.node[edge[1]]['label']) + "`) WHERE a.id=" + str(edge[0]) + " AND b." + str(edge[1]) + " CREATE(a)-[:PPI]->(b)"
+            print(cqlQuery)
+            tx.run(cqlQuery)
+            # MATCH(a:`29`), (b:`25`) WHERE a.id=0 AND b.id=1773 CREATE(a)-[:PPI]->(b)
