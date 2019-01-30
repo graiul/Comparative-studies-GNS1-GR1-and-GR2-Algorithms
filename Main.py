7eec3f5a32313ee3aafb48301d08271213a2b4e0
DESKTOP-FOTVVAB\Gheorghica Radu <radugheorghe715@gmail.com>
2019-01-30 23:39:57 +0200

    Pot rula inca o metoda in care am acces prin tranzactia tx.run la baza de date? Nu puteam crea o metoda noua care sa poata rula, spunand ca are un parametru lipsa, dar cealalta functiona...
    La un moment dat nu mai adauga arce in timpul rularii
    
    Incerc sa returnez nodurile adiacente, dar deocamdata pot returna doar un nod cautand id-ul RI - Cloud.Load din p788.zhaosun. A nu se face confuzie intre id-uri ale nodurilor grafului RI si id-urile date nodurilor de catre Neo4j la inserare.
    Aici am incercat si cu biblioteca APOC - call apoc.neighbors.tohop()

--- Main.py
+++ Main.py
@@ -33,5 +33,26 @@ print(list(list(graph.nodes(data=True))[0][1].values()))
 print(list(graph.nodes(data=True))[0][0])
 # for node in graph.nodes(data=True):
 #     print(node)
+# for edge in graph.edges:
+#     print(edge)
 test = neo4j_test("bolt://localhost:7687", "neo4j", "graph")
 test.insert_graph_in_db(graph)
+# test.create_neo4j_graph_edges(graph)
+
+# Sterge tot:
+# Cypher: MATCH (n) DETACH DELETE n
+# Afiseaza tot
+# MATCH (n) RETURN (n)
+# Insereaza un arc:
+# MATCH(a:`29`), (b:`25`) WHERE a.id=0 AND b.id=1773 CREATE(a)-[:PPI]->(b)
+# Sterge toate arcele:
+# MATCH ()-[r:PPI]-() DELETE r
+
+# Incercare de returnare noduri adiacente unui nod dat - pt Cloud.Load din p788.zhaosun.
+# call apoc.neighbors.tohop(call apoc.node.id(1431), 'PPI', 1)
+# call apoc.nodes.get(1431), neo4j id...
+
+# Pot rula inca o metoda in care am acces prin tranzactia tx.run la baza de date? Nu puteam crea o metoda noua care sa poata rula, spunand ca are un parametru lipsa, dar cealalta functiona...
+# La un moment dat nu mai adauga arce in timpul rularii
+# Incerc sa returnez nodurile adiacente, dar deocamdata pot returna doar un nod cautand id-ul RI - Cloud.Load din p788.zhaosun. A nu se face confuzie intre id-uri ale nodurilor grafului RI si id-urile date nodurilor de catre Neo4j la inserare.
+#   Aici am incercat si cu biblioteca APOC - call apoc.neighbors.tohop()
--- neo4j_test.py
+++ neo4j_test.py
@@ -26,16 +26,28 @@ class neo4j_test(object):
 
     @staticmethod
     def create_neo4j_graph_node(tx, graph):
-        for node in list(graph.nodes(data=True)):
+        # for node in list(graph.nodes(data=True)):
             # tx.run("CREATE (a:Node {id: $id, label: $label})", id=node[0], label=list(list(node[1].values())))
             # print(str(list(list(node[1].values()))[0]))
-            cqlQuery = "CREATE (a:`" + str(list(list(node[1].values()))[0]) + "` {id: " + str(node[0]) + "})"
-            tx.run(cqlQuery)
+            # cqlQuery = "CREATE (a:`" + str(list(list(node[1].values()))[0]) + "` {id: " + str(node[0]) + "})"
+            # tx.run(cqlQuery)
+
+        for edge in graph.edges():
+            cqlQuery = "MATCH(a:`" + str(graph.node[edge[0]]['label']) + "`), (b:`" + str(graph.node[edge[1]]['label']) \
+                       + "`) WHERE a.id=" + str(edge[0]) + " AND b.id=" + str(edge[1]) + " CREATE(a)-[:PPI]->(b)" # Se blocheaza la inserarea unui arc anume: MATCH(a:`32`), (b:`15`) WHERE a.id=12036 AND b.id=12174 CREATE(a)-[:PPI]->(b)
+            # print(cqlQuery)
+            try:
+                tx.run(cqlQuery)
+                print("Inserare arc cu succes!")
+                print(cqlQuery)
+            except:
+                print("Nu a fost inserat arcul!")
 
     @staticmethod
-    def create_neo4j_graph_edges(tx, graph):
-        for edge in graph.edges():
-            cqlQuery = "MATCH(a:`" + str(graph.node[edge[0]]['label']) + "`), (b:`" + str(graph.node[edge[1]]['label']) + "`) WHERE a.id=" + str(edge[0]) + " AND b." + str(edge[1]) + " CREATE(a)-[:PPI]->(b)"
-            print(cqlQuery)
-            tx.run(cqlQuery)
-            # MATCH(a:`29`), (b:`25`) WHERE a.id=0 AND b.id=1773 CREATE(a)-[:PPI]->(b)
+    def Cloud_Load(tx, id): # Pot rula inca o metoda?
+        cqlQuery = "MATCH (n) WHERE n.id = " + str(id) + " RETURN n" # Merge cautarea unui nod dupa id.
+        result = tx.run(cqlQuery)
+        nodes_loaded = []
+        nodes_loaded.append(result)
+        cqlQuery = "match(n:`1`{id:825})--(c) return c" # ??? https://maxdemarzi.com/2018/10/01/finding-your-neighbors-using-neo4j/
+        return result
