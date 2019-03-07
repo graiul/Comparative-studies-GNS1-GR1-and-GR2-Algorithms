6a87491991977d6a7a8715586ee149b444ddc381
DESKTOP-FOTVVAB\Gheorghica Radu <radugheorghe715@gmail.com>
2019-03-07 12:27:58 +0200

    Algoritmul 2 STwig-Order-Selection(q) Zhaosun pe jumatate finalizat.

--- neo4j_test_2.py
+++ neo4j_test_2.py
@@ -6,17 +6,18 @@ from py2neo import Graph, Node, Relationship
 import networkx as nx
 import copy
 import numpy
+import operator
 import itertools
 
 class neo4j_test_2(object):
 
-    neograph = Graph(port="7687", user="neo4j", password="graph")
-    # neograph = Graph(port="11004", user="neo4j", password="graph")
+    neograph_data = Graph(port="7687", user="neo4j", password="graph") # Data Graph Zhaosun
+    neograph_query = Graph(port="11004", user="neo4j", password="graph") # Query Graph Zhaosun
 
 
     def Cloud_Load(self, RI_id):  # Pot rula inca o metoda?
         # print("Cloud_Load:")
-        tx = self.neograph.begin()
+        tx = self.neograph_data.begin()
         # cqlQuery = "MATCH (n) WHERE n.RI_id = " + str(RI_id) + " RETURN n"  # Merge cautarea unui nod dupa id.
         cqlQuery = "MATCH (n) WHERE n.id = " + str(RI_id) + " RETURN n"  # Merge cautarea unui nod dupa id.
         # print(cqlQuery)
@@ -48,7 +49,7 @@ class neo4j_test_2(object):
         return biglist
 
     def Index_getID(self, label):
-        tx = self.neograph.begin()
+        tx = self.neograph_data.begin()
         # cqlQuery = "MATCH (n:`" + str(label) + "`) RETURN n.RI_id"
         cqlQuery = "MATCH (n:`" + str(label) + "`) RETURN n.id" # IF a IN a1! Graf Zhaosun
 
@@ -60,7 +61,7 @@ class neo4j_test_2(object):
 
     # def Index_hasLabel(self, RI_id, label):
     def Index_hasLabel(self, id, label):
-        tx = self.neograph.begin()
+        tx = self.neograph_data.begin()
         # cqlQuery = "MATCH (n:`" + str(label) + "`) WHERE n.RI_id=" + str(RI_id) + " RETURN n"
         cqlQuery = "MATCH (n:`" + str(label) + "`) WHERE n.id='" + str(id) + "' RETURN n"
         # print(cqlQuery)
@@ -151,12 +152,13 @@ class neo4j_test_2(object):
                 # print(stwig)
                 R.append(stwig)
         print("STWIGS: ")
-        for stwig in sorted(R):
+        STwigs = sorted(R)
+        for stwig in STwigs:
             print(stwig)
         # Am schimbat graful astfel: am inlaturat muchia a3,b3: MATCH (n:a)-[r:RELTYPE]-(m:b) WHERE n.id = 'a3' AND m.id = 'b3' DELETE r
         #                            am adaugat muchia a3,c3: # MATCH (n:a),(m:c) WHERE n.id = 'a3' AND m.id = 'c3' CREATE (n)-[r:RELTYPE]->(m)
         # Astfel am obtinur rezultatele din p788_Zhaosun, pag 5, G(q1) = ...
-
+        return STwigs
 
     def Query_Graph_Split(self, query_graph):
 
@@ -187,6 +189,49 @@ class neo4j_test_2(object):
         print("Splits: ")
         print(splits)
 
+    def f_value(self, v_id, query_graph):
+        deg = len(list(query_graph.neighbors(v_id)))
+        # print("deg: " + str(deg))
+        # print("neighbors: " + str(list(query_graph.neighbors(v))))
+        # print(query_graph.node[v_id]['label'])
+        return deg / self.freq(query_graph.node[v_id]['label'])
+
+    def freq(self, v_label):
+        # Pentru Data Graph Zhaosun
+        tx = self.neograph_data.begin()
+        cqlQuery = "MATCH (n:`" + str(v_label) + "`) RETURN n"
+        result = tx.run(cqlQuery).to_ndarray()
+        return len(list(result))
+
+
+    def STwig_Order_Selection(self, query_graph):
+        S = []
+        T = None
+        dict_f_values_query_graph = {}
+        picked_edge = None
+        while len(query_graph.edges()) > 0:
+            for edge in query_graph.edges():
+                sum = self.f_value(edge[0], query_graph) + self.f_value(edge[1], query_graph)
+                print("Sum: " + str(sum))
+                dict_f_values_query_graph[edge] = sum
+            if len(S) == 0:
+
+                print(dict_f_values_query_graph.items())
+                index, value = max(enumerate(list(dict_f_values_query_graph.values())), key=operator.itemgetter(1))
+                print("Max val: " + str(value))
+                print("Max val index: " + str(index))
+                picked_edge = list(dict_f_values_query_graph.keys())[index]
+                print("Picked edge: " + str(picked_edge))
+            else:
+                index2, value2 = max(enumerate(list(dict_f_values_query_graph.values())), key=operator.itemgetter(1))
+                for edge in query_graph.edges():
+                    sum2 = self.f_value(edge[0], query_graph) + self.f_value(edge[1], query_graph)
+                    if edge[0] in S and sum2 == value2:
+                        picked_edge = edge
+                        print("Picked edge on 'else' branch: " + str(picked_edge))
+                        break
+            q = self.Cloud_Load(picked_edge[0])
+            # Tv = self.MatchSTwig(q) # Aceasta este metoda?
 
 
 # #BIG DATA GRAPH FROM RI DB############################################
@@ -265,7 +310,7 @@ query_graph.add_edges_from(query_graph_edges)
 node_attr = ["a", "b", "c", "d", "e", "f"]
 node_attr_dict = dict(zip(sorted(query_graph.nodes()), node_attr))
 nx.set_node_attributes(query_graph, node_attr_dict, 'label')
-# print(query_graph.nodes(data=True))
+print(query_graph.nodes(data=True))
 
 # for split in test2.Query_Graph_Split(query_graph):
 #     print(split)
@@ -274,28 +319,34 @@ nx.set_node_attributes(query_graph, node_attr_dict, 'label')
 
 query_graph_nodes = list(query_graph.nodes())
 print(query_graph_nodes)
-start = 0
-nodes_chunk = 2
-chunks = []
-# for node in query_graph_nodes:
-while start<len(query_graph_nodes):
-    chunks.append(query_graph_nodes[start:nodes_chunk])
-    start = start + 2
-    nodes_chunk = nodes_chunk + 2
-print(chunks)
-threads = []
-for i in range(len(chunks)):
-    thread = threading.Thread(target=test2.Query_Graph_Split_Parallel, args=[chunks[i]], name="Thread " + str(i))
-    threads.append(thread)
 
-for thread in threads:
-    print()
-    print(thread.name)
-    thread.start()
-    thread.join()
+# start = 0
+# nodes_chunk = 2
+# chunks = []
+# # for node in query_graph_nodes:
+# while start<len(query_graph_nodes):
+#     chunks.append(query_graph_nodes[start:nodes_chunk])
+#     start = start + 2
+#     nodes_chunk = nodes_chunk + 2
+# print(chunks)
+# threads = []
+# for i in range(len(chunks)):
+#     thread = threading.Thread(target=test2.Query_Graph_Split_Parallel, args=[chunks[i]], name="Thread " + str(i))
+#     threads.append(thread)
+#
+# for thread in threads:
+#     print()
+#     print(thread.name)
+#     thread.start()
+#     thread.join()
 
+# number_of_threads = int(input("Scrieti numarul de thread-uri: "))
+# nodes_chunk2 = len(query_graph_nodes) / number_of_threads
+# print(nodes_chunk2)
 
-
-# th1 = threading.Thread(target=test2.Query_Graph_Split_Parallel, args=[query_graph_nodes, 0, len(query_graph.edges())], name="Thread 1")
-# print(th1.name)
-# th1.start()
+print("Freq: ")
+print(test2.freq("a"))
+print("f_value: ")
+print(test2.f_value("a1", query_graph))
+print("STwig_Order_Selection: ")
+print(test2.STwig_Order_Selection(query_graph))
