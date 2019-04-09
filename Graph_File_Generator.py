c4f1026406e7942edc8facd17d0daec3b5ff216e
DESKTOP-FOTVVAB\Gheorghica Radu <radugheorghe715@gmail.com>
2019-04-09 19:47:14 +0300

    In Graph_File_Generator generez folosind NetworkX graful query Zhaosun.

---
+++ Graph_File_Generator.py
@@ -0,0 +1,12 @@
+import networkx as nx
+
+class Graph_File_Generator(object):
+    # Zhaosun Query Graph
+    def gen_zhaosun_query_graph(self):
+        query_graph = nx.Graph()
+        query_graph_edges = [["a1", "b1"], ["a1", "c1"], ["c1", "d1"], ["c1", "f1"], ["f1", "d1"], ["d1", "b1"], ["d1", "e1"], ["e1", "b1"]]
+        query_graph.add_edges_from(query_graph_edges)
+        node_attr = ["a", "b", "c", "d", "e", "f"]
+        node_attr_dict = dict(zip(sorted(query_graph.nodes()), node_attr))
+        nx.set_node_attributes(query_graph, node_attr_dict, 'label')
+        return query_graph
