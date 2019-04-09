08ed8f9ed01be87ba0bf5e23d45f50d87cdc84e5
DESKTOP-FOTVVAB\Gheorghica Radu <radugheorghe715@gmail.com>
2019-04-09 09:06:44 +0300

    Paralelizat citirea dintr-un singur READ_REPLICA. Mai multe threaduri per READ_REPLICA. O singura READ_REPLICA este de ajuns.

--- Local_Cluster_Access_Test.py
+++ Local_Cluster_Access_Test.py
@@ -1,8 +1,26 @@
+import threading
+
 from py2neo import Graph, Node, Relationship
 
 class Local_Cluster_Access_Test(object):
-    neograph_data = Graph("bolt://127.0.0.1:7693", auth=("neo4j", "changeme"))  # Data Graph Zhaosun
-    tx = neograph_data.begin()
-    cqlQuery = "MATCH (n) WHERE n.name = 'Andy' RETURN n"
-    result = tx.run(cqlQuery).to_ndarray()
-    print(result)
+    neograph_data = Graph("bolt://127.0.0.1:7693", auth=("neo4j", "changeme"))
+    def parallel_access_to_one_read_replica(self, query):
+        tx = self.neograph_data.begin()
+        # cqlQuery = "MATCH (n) WHERE n.name = 'Andy' RETURN n"
+        # cqlQuery = "MATCH (n) RETURN n"
+        cqlQuery = query
+        result = tx.run(cqlQuery).to_ndarray()
+        print(result)
+
+test = Local_Cluster_Access_Test()
+thread_list = []
+queries = [["MATCH (n) WHERE n.name = 'Andy' RETURN n"], ["MATCH (n) RETURN n"]]
+for q in queries:
+    thread = threading.Thread(target=test.parallel_access_to_one_read_replica, args=q, name="Thread " + str(q[0]))
+    thread_list.append(thread)
+
+for thread in thread_list:
+    print()
+    print(thread.name)
+    thread.start()
+    thread.join()
