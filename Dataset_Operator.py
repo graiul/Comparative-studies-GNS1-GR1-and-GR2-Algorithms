cb2c64a041b386a5542d4356bbf3842b6e419f7b
DESKTOP-FOTVVAB\Gheorghica Radu <radugheorghe715@gmail.com>
2019-04-09 22:58:15 +0300

    In Dataset_Operator am adaugat operatii de inserare noduri si muchii/arce in nodul lider al clusterului neo4j.

---
+++ Dataset_Operator.py
@@ -0,0 +1,39 @@
+from py2neo import Graph, Node, Relationship
+
+class Dataset_Operator(object):
+    dataset_nodes_url = None
+    dataset_edges_url = None
+    leader_core_bolt_address = None
+    username = None
+    passwd = None
+    def __init__(self, dataset_nodes_url, dataset_edges_url, leader_core_bolt_address, username, passwd):
+        self.dataset_nodes_url = dataset_nodes_url
+        self.dataset_edges_url = dataset_edges_url
+        self.leader_core_bolt_address = leader_core_bolt_address
+        self.username = username
+        self.passwd = passwd
+
+    def insert_nodes(self):
+        neograph_data = Graph(self.leader_core_bolt_address, auth=(self.username, self.passwd))
+        # tx = neograph_data.begin() # LA VARIANTA CU NEO4J CA SI CLUSTER NU MERGE, NEO4j VER 3.3.1
+        cqlQuery = "LOAD CSV WITH HEADERS FROM '" + str(self.dataset_nodes_url) + "' AS line" \
+                   " CREATE (:Node {  zhaosun_id: line.zhaosun_id, zhaosun_label: line.zhaosun_label})"
+        # tx.run(cqlQuery)
+        neograph_data.run(cqlQuery)
+
+    def insert_edges(self):
+        neograph_data = Graph(self.leader_core_bolt_address, auth=(self.username, self.passwd))
+        # tx = neograph_data.begin()
+        cqlQuery = "LOAD CSV WITH HEADERS FROM '" + str(self.dataset_edges_url) + "' AS line" \
+                   " MERGE (n:Node {zhaosun_id: line.zhaosun_from})" \
+                   " MERGE (m:Node {zhaosun_id: line.zhaosun_to})" \
+                   " MERGE (n)-[:PPI]-(m)"
+        # tx.run(cqlQuery)
+        neograph_data.run(cqlQuery)
+
+    def delete_data_from_cluster(self):
+        neograph_data = Graph(self.leader_core_bolt_address, auth=(self.username, self.passwd))
+        # tx = neograph_data.begin()
+        cqlQuery = "MATCH (n) DETACH DELETE n"
+        # tx.run(cqlQuery)
+        neograph_data.run(cqlQuery)
