7e7d6b3646f7c38ad46a3dc212ebc02c9e1b9f3e
Gheorghica Radu <radugheorghe715@gmail.com>
2019-05-30 14:57:18 +0300

    Adaugat afisare id-uri pentru fiecare proces creat, cat si obtinerea de nuclee disponibile(8) pentru a crea un pool de procese direct cu acest nr daca este necesar.

--- Multiprocessing_Test.py
+++ Multiprocessing_Test.py
@@ -1,5 +1,5 @@
+import multiprocessing as mp
 from multiprocessing import Pool, Process, Queue
-# from pathos.multiprocessing import ProcessingPool as Pool
 
 # from py2neo import Graph, Node, Relationship
 from DB_Access_Test import DB_Access_Test
@@ -15,6 +15,7 @@ class Foo():
         neograph_data = Graph("bolt://127.0.0.1:7690", auth=("neo4j", "changeme"))
         tx = neograph_data.begin()
         cqlQuery = query
+        print(mp.current_process())
         start_time = timer()
         result = tx.run(cqlQuery).to_ndarray()
         total_time_sec = timer() - start_time
@@ -36,6 +37,8 @@ if __name__ == '__main__':
     foo = Foo()
     db = DB_Access_Test()
     queries = ["MATCH (n) RETURN n", "MATCH (n) RETURN n", "MATCH (n) RETURN n"]
+    pool_size = mp.cpu_count()
+    print(pool_size)
     p = Pool(3)
     print("Pool result:")
     start_time = timer()
@@ -45,11 +48,12 @@ if __name__ == '__main__':
     p.join()
     total_time_sec = timer() - start_time
     total_time_millis = total_time_sec * 1000
-    print(res.get())
-    # for r in res:
-    #     for rr in r:
-    #         print(rr)
-    #     print()
+    # print(res.get())
+    print()
+    for r in res.get():
+        for rr in r:
+            print(rr)
+        print()
     print()
     print('\x1b[0;30;45m' + 'DB_Access_Test multiple procs total exec time: ' + str(
         total_time_millis) + ' ms' + '\x1b[0m')
