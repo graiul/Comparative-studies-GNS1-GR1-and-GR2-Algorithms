0ce6bd9be97891505d404594f750ef4e8a382665
Gheorghica Radu <radugheorghe715@gmail.com>
2019-07-05 21:45:05 +0300

    Backtracking

--- Backtracking_STwig_Matching.py
+++ Backtracking_STwig_Matching.py
@@ -14,14 +14,53 @@ import networkx as nx
 # print(permute(2, ["a","b","c"]))
 
 
-# def permute(list, s):
-#     if list == 1:
-#         return s
-#     else:
-#         return [ y + x
-#                  for y in permute(1, s)
-#                  for x in permute(list - 1, s)
-#                  ]
+def match_stwig_backtracking(query_stwig, query_stwig_as_labels, data_graph, index, solution):
+# Cate o lista cu noduri din graf pentru fiecare tip de nod din STwig. Atunci lucram doar cu cateva liste.
+
+
+# Verificam daca exista solutie
+#   Cum returnam mai multe solutii? Cum facem intoarcerea?
+# Punem criteriul de validitate
+# In caz de validitate, apelam din nou si construim astfel solutia
+
+
+    # if solution == [[]] or len(solution[1]) == len(query_stwig[1]):
+    if len(solution) > 1:
+        if len(solution[1]) == len(query_stwig[1]):
+            print("complete solution: " + str(solution))
+    else:
+        # Pentru radacina STwig-ului:
+        if len(solution) == 1:
+            print(len(solution))
+            for root in data_graph.nodes():
+                print("root: " + str(root))
+                if data_graph.node[root]['label'] == query_stwig_1_as_labels[0]: # Avem un root al unei solutii
+                    solution.insert(0,root)
+                    # solution.append([])
+                    print("solution start: " + str(solution))
+                    match_stwig_backtracking(query_stwig, query_stwig_as_labels, data_graph, index, solution)
+                    break
+                    # La urma vom sterge radacina, si vom intra din nou pe ramura aceasta.
+                    # Trebuie facut ca sa nu ia aceeasi radacina din nou.
+
+        if len(solution) > 1:
+            for leaf in data_graph.nodes():
+                print("-leaf: " + str(leaf))
+                for leaf_label in query_stwig_as_labels[1]:
+                    print("leaf_label: " + str(leaf_label))
+                    if data_graph.node[leaf]['label'] == leaf_label: # Avem un nod te tipul unui leaf
+                        print("---YES label")
+                        if data_graph.has_edge(solution[0], leaf): # Verificam daca este vecinatate de ordinul 1
+                            solution[1].append(leaf)
+                            print("solution: " + str(solution))
+                            match_stwig_backtracking(query_stwig, query_stwig_as_labels, data_graph, index, solution)
+                            return solution
+
+        # for root_label in root_label_nodes_dict.keys():
+        #     for root in root_label_nodes_dict.get(root_label):
+        #         solution.append(root)
+        #         for leaf_label in leaf_labels_nodes_dict
+
 
 
 # Cream graful de 1000 de muchii.
@@ -55,7 +94,78 @@ print(aux_graph.nodes(data=True))
 print(len(aux_graph.nodes(data=True)))
 
 graph_for_bactracking_search = aux_graph
+query_stwig_1 = ['1773', ['1488', '1898', '2285']]
+# Label-ul radacinii
+root_label = graph_for_bactracking_search.node[query_stwig_1[0]]['label']
+# Label-urile vecinilor din lista
+neighbor_labels = []
+for n in query_stwig_1[1]:
+    neighbor_labels.append(graph_for_bactracking_search.node[n]['label'])
+query_stwig_1_as_labels = [root_label, neighbor_labels]
+print("query_stwig_1_as_labels: " + str(query_stwig_1_as_labels))
+match_stwig_backtracking(query_stwig_1, query_stwig_1_as_labels, graph_for_bactracking_search, 0, [[]])
 
 
+# root_label_nodes_dict = {}
+# for node in graph_for_bactracking_search.nodes():
+#     if graph_for_bactracking_search.node[node]['label'] == query_stwig_1_as_labels[0]:
+#         root_label_nodes_dict[query_stwig_1_as_labels[0]] = node
+#
+#
+# leaf_labels_nodes_dict = {}
+# if len(query_stwig_1_as_labels[1]) == 1:
+#     leaf_label_1_nodes = []
+#
+#     for node in graph_for_bactracking_search.nodes():
+#         if graph_for_bactracking_search.node[node]['label'] == query_stwig_1_as_labels[1][0]:
+#             leaf_label_1_nodes.append(node)
+#     leaf_labels_nodes_dict[query_stwig_1_as_labels[1][0]] = leaf_label_1_nodes
+#
+# elif len(query_stwig_1_as_labels[1]) == 2:
+#     leaf_label_1_nodes = []
+#     leaf_label_2_nodes = []
+#     for node in graph_for_bactracking_search.nodes():
+#         if graph_for_bactracking_search.node[node]['label'] == query_stwig_1_as_labels[1][0]:
+#             leaf_label_1_nodes.append(node)
+#         elif graph_for_bactracking_search.node[node]['label'] == query_stwig_1_as_labels[1][1]:
+#             leaf_label_2_nodes.append(node)
+#     leaf_labels_nodes_dict[query_stwig_1_as_labels[1][0]] = leaf_label_1_nodes
+#     leaf_labels_nodes_dict[query_stwig_1_as_labels[1][1]] = leaf_label_2_nodes
+#
+#
+# elif len(query_stwig_1_as_labels[1]) == 3:
+#     leaf_label_1_nodes = []
+#     leaf_label_2_nodes = []
+#     leaf_label_3_nodes = []
+#     for node in graph_for_bactracking_search.nodes():
+#         if graph_for_bactracking_search.node[node]['label'] == query_stwig_1_as_labels[1][0]:
+#             leaf_label_1_nodes.append(node)
+#         elif graph_for_bactracking_search.node[node]['label'] == query_stwig_1_as_labels[1][1]:
+#             leaf_label_2_nodes.append(node)
+#         elif graph_for_bactracking_search.node[node]['label'] == query_stwig_1_as_labels[1][2]:
+#             leaf_label_3_nodes.append(node)
+#     leaf_labels_nodes_dict[query_stwig_1_as_labels[1][0]] = leaf_label_1_nodes
+#     leaf_labels_nodes_dict[query_stwig_1_as_labels[1][1]] = leaf_label_2_nodes
+#     leaf_labels_nodes_dict[query_stwig_1_as_labels[1][2]] = leaf_label_3_nodes
+#
+#
+# elif len(query_stwig_1_as_labels[1]) == 4:
+#     leaf_label_1_nodes = []
+#     leaf_label_2_nodes = []
+#     leaf_label_3_nodes = []
+#     leaf_label_4_nodes = []
+#     for node in graph_for_bactracking_search.nodes():
+#         if graph_for_bactracking_search.node[node]['label'] == query_stwig_1_as_labels[1][0]:
+#             leaf_label_1_nodes.append(node)
+#         elif graph_for_bactracking_search.node[node]['label'] == query_stwig_1_as_labels[1][1]:
+#             leaf_label_2_nodes.append(node)
+#         elif graph_for_bactracking_search.node[node]['label'] == query_stwig_1_as_labels[1][2]:
+#             leaf_label_3_nodes.append(node)
+#         elif graph_for_bactracking_search.node[node]['label'] == query_stwig_1_as_labels[1][3]:
+#             leaf_label_4_nodes.append(node)
+#     leaf_labels_nodes_dict[query_stwig_1_as_labels[1][0]] = leaf_label_1_nodes
+#     leaf_labels_nodes_dict[query_stwig_1_as_labels[1][1]] = leaf_label_2_nodes
+#     leaf_labels_nodes_dict[query_stwig_1_as_labels[1][2]] = leaf_label_3_nodes
+#     leaf_labels_nodes_dict[query_stwig_1_as_labels[1][3]] = leaf_label_4_nodes
 
 
