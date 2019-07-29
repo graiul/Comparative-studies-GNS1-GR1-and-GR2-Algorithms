4c450e82b73a9bf3e7d0c686ecd73082b54d16af
Gheorghica Radu <radugheorghe715@gmail.com>
2019-07-29 13:54:13 +0300

    Adaugat metoda pentru generarea unui query graph mic [1,2,3] cu label-urile [a,b,c] pentru testarea cu graful cu zece noduri si patru label-uri.

--- Graph_File_Generator.py
+++ Graph_File_Generator.py
@@ -22,3 +22,19 @@ class Graph_File_Generator(object):
         node_attr_dict = dict(zip(sorted(query_graph.nodes()), node_attr))
         nx.set_node_attributes(query_graph, node_attr_dict, 'label')
         return query_graph
+
+    def gen_small_graph_query_graph(self):
+        small_graph = nx.Graph()
+        small_graph_nodes = [1, 2, 3]
+        # Sortarea ascendenta la string este diferita de cea a de la tipul int
+        small_graph_nodes.sort()
+        small_graph_edges = [[1, 2], [1, 3]]
+        small_graph.add_nodes_from(small_graph_nodes)
+        small_graph.add_edges_from(small_graph_edges)
+        node_attr = ["a", "b", "c"]
+        node_attr_dict = dict(zip(sorted(small_graph.nodes()), node_attr))
+        # print(node_attr_dict.items())
+        nx.set_node_attributes(small_graph, node_attr_dict, 'label')
+        # print(small_graph.nodes(data=True))
+        # print(small_graph.edges())
+        return small_graph
