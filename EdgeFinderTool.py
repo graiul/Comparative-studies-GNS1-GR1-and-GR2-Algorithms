22e74aa0584d03f0664ff21c7e6eb1c72cd77e06
Gheorghica Radu <radugheorghe715@gmail.com>
2020-02-12 19:58:30 +0200

    Finalizat si functional pentru cate o muchie ca si paramentru de intrare.

--- EdgeFinderTool.py
+++ EdgeFinderTool.py
@@ -2,8 +2,10 @@ class EdgeFinderTool(object):
     input_edge = None
     list_of_edges = []
     def __init__(self, input_edge, list_of_edges):
+    # def __init__(self, input_edge, list_of_edges, data_graph):
         self.input_edge = input_edge
         self.list_of_edges = list_of_edges
+        # self.data_graph = data_graph
 
     def edge_found(self):
         if len(self.list_of_edges) == 0:
@@ -14,9 +16,9 @@ class EdgeFinderTool(object):
                 node1 = self.input_edge[0]
                 node2 = self.input_edge[1]
                 if self.input_edge[0] in edge and self.input_edge[1] in edge:
-                    # found = True
+                    # if self.data_graph.nodes[self.input_edge[0]]['label'] == self.data_graph.nodes[edge[0]]['label'] and self.data_graph.nodes[self.input_edge[1]]['label'] == self.data_graph.nodes[edge[1]]['label']:
+
                     return True
                 else:
                     found = False
             return found
-
