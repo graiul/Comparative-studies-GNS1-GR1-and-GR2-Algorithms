ba0000253359a096ac9ca1533eef58de608808a3
Gheorghica Radu <radugheorghe715@gmail.com>
2020-04-23 15:33:10 +0300

    Finalizat calculator de medie aritmetica avand ca si date de intrare valori float(care in PY sunt echivalentul lui double din alte limbaje) din fisier text.

--- Average_time_calculator.py
+++ Average_time_calculator.py
@@ -1,12 +1,31 @@
-class Average_time_calculator(object):
-    list_of_execution_times = []
-    times_sum = 0
-    result = 0
-    def __init__(self, list_of_execution_times):
-        self.list_of_execution_times = list_of_execution_times
-    def get_execution_times_average(self):
-        length = len(self.list_of_execution_times)
-        for t in self.list_of_execution_times:
-            self.times_sum = self.times_sum + t
-        self.result = self.times_sum / length
-        return self.result
+# f1 = open("file_STwig Algorithm execution times.txt", "r+")
+f1 = open("file_VF2 Algorithm execution times.txt", "r+")
+
+list_of_execution_times = []
+times_sum = 0
+result = 0
+f1_string_lines = []
+
+# with open("file_STwig Algorithm execution times.txt", "r") as rd:
+with open("file_VF2 Algorithm execution times.txt", "r") as rd:
+    # Read lines in loop
+    for line in rd:
+        # All lines (besides the last) will include  newline, so strip it
+        f1_string_lines.append(line.strip())
+
+f1_int_lines = []
+float_val_lines = []
+for string_line in f1_string_lines:
+    # https://www.geeksforgeeks.org/python-string-split/
+    split_string_line = string_line.split(" ")
+    for string_backtracking_line_element in split_string_line:
+        float_val_lines.append(float(string_backtracking_line_element))
+
+print(float_val_lines)
+times_sum = float(0)
+result = float(0)
+length = len(float_val_lines)
+for t in float_val_lines:
+    times_sum = times_sum + t
+result = times_sum / length
+print(result)
