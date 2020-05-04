8e6896e136b3dd378bf4ec527b8f469671aacaf2
Gheorghica Radu <radugheorghe715@gmail.com>
2020-05-04 19:23:26 +0300

    Pregatit baterie de teste pentru STwig Algoritm, VF2 Algorithm si GNS1 Algorithm.

--- Query_Graph_Generator.py
+++ Query_Graph_Generator.py
@@ -52,42 +52,72 @@ class Query_Graph_Generator(object):
 
         # Grafuri query cu 3 noduri:
 
+        # ok
         # query_graph_edges = [[7711, 2243], [7711, 2259]]
         # node_attr = ["16", "2", "17"]
 
+        # ok
         # query_graph_edges = [[2670, 10109], [2670, 10387]]
         # node_attr = ["29", "30", "20"]
 
+        # ok
         # query_graph_edges = [[4164, 3526], [4164, 5687]]
         # node_attr = ["10", "26", "2"]
 
+        # ok
         # query_graph_edges = [[5636, 2904], [5636, 3414]]
         # node_attr = ["16", "6", "30"]
 
-        # query_graph_edges = [[10553, 7888], [10553, 8186]]
-        # node_attr = ["3", "27", "27"]
+        # ok
+        # query_graph_edges = [[11965, 1278], [11965, 1291]]
+        # node_attr = ["16", "13", "9"]
 
 
         # Grafuri query cu 6 noduri:
 
+        # ok
         # query_graph_edges = [[11041, 2467], [11041, 2607], [11041, 2650], [11041, 2904], [11041, 3414]]
         # node_attr = ["18", "23", "11", "9", "6", "30"]
 
+        # ok
         # query_graph_edges = [[7563, 5755], [7563, 6256], [7563, 6784], [7563, 7289], [7563, 7308]]
         # node_attr = ["18", "23", "3", "15", "26", "2"]
 
+        # ok
         # query_graph_edges = [[6880, 5687], [6880, 6392], [6880, 9094], [6880, 12206], [6880, 11000]]
-        # node_attr = [["9", "2", "23", "10", "22", "24"]]
+        # node_attr = ["9", "2", "23", "10", "22", "24"]
 
-        # query_graph_edges = [[7190, 2900], [7190, 3255], [7190, 3258], [7190, 3411], [7190, 3586]]
-        # node_attr = ["3", "5", "24", "11", "5", "14"]
+        # nu a fost gata nici dupa o executie de cinci ore si jumatate
+        # query_graph_edges = [[7190, 3586], [7190, 3857], [7190, 3937], [7190, 4344], [7190, 4396]]
+        # node_attr = ["3", "14", "8", "16", "11", "13"]
 
-        # query_graph_edges = [[12472, 10543], [12472, 10688], [12472, 10752], [12472, 10808], [12472, 10978]]
-        # node_attr = ["8", "8", "5", "25", "15", "14"]
+        # ok
+        # query_graph_edges =[[58, 1870], [58, 1878], [58, 1982], [58, 3396], [58, 4819]]
+        # node_attr = ["20", "13", "32", "4", "14", "11"]
+
+        # ok
+        # query_graph_edges = [[979, 4337], [979, 4341], [979, 4419], [979, 4736], [979, 4888]]
+        # node_attr = ["14", "13", "3", "28", "8", "18"]
 
 
         # Grafuri query cu 9 noduri:
 
+        # Acest graf blocheaza laptop-ul - STwig alg
+        # query_graph_edges = [[10881, 8738], [10881, 8962], [10881, 9854], [10881, 10074], [10881, 10527], [10881, 10699], [10881, 11131], [10881, 12422]]
+        # node_attr = ["18", "32", "1", "29", "20", "8", "17", "21", "2"]
+
+        # Dupa 12 ore nu a terminat executia - STwig alg. Acesta este graful 11 din articolul 2.
+        # Am incercat o noua executie: 28 apr 2020 ora 930 - 30 apr 2020 ora 1533 si a dat MemoryError la sfarsit. Mesajul complet de eroare cat si acest STwig input este aflat intr-un fisier text in directorul articolului 2.
+        # query_graph_edges = [[3719, 850], [3719, 1287], [3719, 2035], [3719, 2611], [3719, 2764], [3719, 3366], [3719, 3482], [3719, 4005]]
+        # node_attr = ["29", "32", "21", "19", "28", "17", "8", "6", "27"]
+
+
+        # ------------------------------------------------------------------------------------------------------------------------------------
+        # Am testat cu 9 si cu 8 noduri, fara sa returneze rezultate in timp fezabil.
+        # query_graph_edges = [[8214, 2598], [8214, 2614], [8214, 2670], [8214, 3238], [8214, 3253], [8214, 3366], [8214, 3620]] #, [8214, 3963]]
+        # node_attr = ["15", "19", "32", "29", "11", "13", "8", "27"] #, "18"]
+
+        # ------------------------------------------------------------------------------------------------------------------------------------
         # query_graph_edges = [[531,5399], [531,5671], [531,6393], [531,6702], [531,7289], [531,7421], [531,7438], [531,8066]]
         # node_attr = ["20", "32", "20", "22", "31", "26", "9", "16", "18"]
 
@@ -100,8 +130,95 @@ class Query_Graph_Generator(object):
         # query_graph_edges = [[7020,2430], [7020,2473], [7020,2508], [7020,2540], [7020,2560], [7020,2573], [7020,2724], [7020,2771]]
         # node_attr = ["21", "9", "18", "24", "16", "26", "21", "6", "21"]
 
-        query_graph_edges = [[11000,8269], [11000,8429], [11000,8507], [11000,9430], [11000,10327], [11000,10426], [11000,11319], [11000,11783]]
-        node_attr = ["24", "19", "32", "25", "32", "24", "26", "11", "12"]
+        # query_graph_edges = [[11000,8269], [11000,8429], [11000,8507], [11000,9430], [11000,10327], [11000,10426], [11000,11319], [11000,11783]]
+        # node_attr = ["24", "19", "32", "25", "32", "24", "26", "11", "12"]
+
+
+        # Grafuri pentru testarea algoritmilor VF2 si GNS2v1 pentru Articolul 3.
+
+        # query_graph_edges = [[1, 2], [3, 4], [5, 6], [7, 8]] # GNS ca si da rezultat o gasire, dar nu stiu daca  mai sunt si alte gasiri existente in graful RI cu 10000 muchii si 4652 de noduri.
+                                                               # STwig Alg si VF2 Alg nu pot lucra cu grafuri query non-STwig.
+                                                               # Comparatia atunci ar putea fi facuta cu un alt algoritm luat de pe net si rulat - pt grafuri query non-STwig
+        # -------------------
+        # query_graph_edges = [[1, 4], [3, 4], [5, 6], [7, 8]] - defect
+        # query_graph.add_node(2)
+        # -------------------
+
+        # node_attr = ["24", "19", "32", "25", "32", "24", "26", "11"]
+
+        # GNS2v1 da bucla infinita, chiar si pentru trei muchii ale acestui graf query cu forma de STwig.
+        # query_graph_edges = [[11041, 2467], [11041, 2607], [11041, 2650]] #, [11041, 2904]] #, [11041, 3414]]
+        # node_attr = ["18", "23", "11", "9"] #, "6"] #, "30"]
+
+        # GNS2v1 bucla infinita pentru patru muchii DAR arata parte din rezultatele returnate de VF2.
+        # Nu stocheaza timpii executiei partiale.
+        # GNS2v1 functional pentru primele trei muchii.
+        # query_graph_edges = [[8028, 3850], [8028, 58], [8028, 7465], [8028, 9294]]
+        # node_attr = ["6", "4", "20", "18", "9"]
+
+        # GNS2v1 Bucla infinita si un singur rezultat.
+        # query_graph_edges = [[2850, 979], [2850, 3526], [2850, 5591]]
+        # node_attr = ["27", "14", "26", "3"]
+
+        # ok
+        # query_graph_edges = [[8028, 3850], [8028, 58], [8028, 7465]]
+        # node_attr = ["6", "4", "20", "18"]
+
+        # ok
+        # query_graph_edges = [[1773, 1488], [1773, 1898], [1773, 2285]]
+        # node_attr = ["25", "28", "29", "27"]
+
+
+
+
+        # Grafuri query cu forma STwig cu patru noduri si trei muchii pentru
+        # testarea algoritmilor  STwig, VF2 si GNS1 pentru Articolul 3.
+
+        # GNS1 ruleaza fara bucla infinita
+        query_graph_edges = [[11041, 2467], [11041, 2607], [11041, 2650]]
+        node_attr = ["18", "23", "11", "9"]
+
+        # GNS1 ruleaza fara bucla infinita
+        # query_graph_edges = [[8028, 3850], [8028, 58], [8028, 7465]]
+        # node_attr = ["6", "4", "20", "18"]
+
+        # GNS1 ruleaza fara bucla infinita
+        # query_graph_edges = [[2850, 979], [2850, 3526], [2850, 5591]]
+        # node_attr = ["27", "14", "26", "3"]
+
+        # GNS1 ruleaza fara bucla infinita
+        # query_graph_edges = [[1773, 1488], [1773, 1898], [1773, 2285]]
+        # node_attr = ["25", "28", "29", "27"]
+
+        # GNS1 ruleaza fara bucla infinita
+        # query_graph_edges = [[0, 1773], [0, 1817],[0,4426]]
+        # node_attr = ["29", "25", "19", "13"]
+
+
+        # Grafuri query cu forma STwig cu trei noduri si doua muchii pentru testarea algoritmilor VF2 si GNS1 pentru Articolul 3.
+
+        # GNS1 ruleaza fara bucla infinita
+        # query_graph_edges = [[2462,4829], [2462,5730]]
+        # node_attr = ["18", "5", "7"]
+
+        # GNS1 ruleaza fara bucla infinita
+        # query_graph_edges = [[11000, 10426], [11000, 11319]]
+        # node_attr = ["24", "26", "11"]
+
+        # GNS1 ruleaza fara bucla infinita
+        # query_graph_edges = [[5157, 580], [5157, 734]]
+        # node_attr = ["32", "18", "17"]
+
+        # GNS1 ruleaza fara bucla infinita
+        # query_graph_edges = [[6954, 7970], [6954, 8161]]
+        # node_attr = ["14", "22", "25"]
+
+        # GNS1 ruleaza fara bucla infinita
+        # query_graph_edges = [[3842, 7591], [3842, 7596]]
+        # node_attr = ["31", "6", "20"]
+
+
+
 
 
         query_graph.add_edges_from(query_graph_edges)
