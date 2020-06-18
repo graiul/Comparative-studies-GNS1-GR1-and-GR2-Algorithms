47fffe7436019784f5f2ec19d2afa24c0cb7d756
Gheorghica Radu <radugheorghica@protonmail.com>
2020-06-18 14:06:29 -0700

    Am pus un print pentru nivelul dincolo de care se repeta producerea ultimei serii de solutii partiale de catre ultimul consumator.

--- Exercitii_Dask_Distributed_cu_grafuri.py
+++ Exercitii_Dask_Distributed_cu_grafuri.py
@@ -95,6 +95,8 @@ def consumer(input_queue, output_queue, query_stwig_leaf_node_label, query_stwig
 
                     if len(partial_solution) == query_stwig_length:
                         print("Consumer " + str(os.getpid()) + ": Partial solution: " + str(partial_solution))
+                        if partial_solution == [3842, 9997, 9670]:
+                            print("!!!")
                         queue_for_printing.put(partial_solution)
 
                     output_queue.put(partial_solution)
