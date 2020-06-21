a99968079fe2a5fec0a821f994ffea7f70cb6887
Gheorghica Radu <radugheorghica@protonmail.com>
2020-06-20 18:47:03 -0700

    Am adaugat un print pentru verificare, pe care mai apoi l-am comentat.

--- Exercitii_Dask_Distributed_cu_grafuri.py
+++ Exercitii_Dask_Distributed_cu_grafuri.py
@@ -97,6 +97,7 @@ def consumer(input_queue, output_queue, query_stwig_leaf_node_label, query_stwig
                     partial_solution.append(data_node)
 
                     if len(partial_solution) == query_stwig_length:
+                        # print("Consumer " + str(os.getpid()) + ": Partial solution: " + str(partial_solution))
 
                         if partial_solution not in aux_partial_solutions_list:
                             queue_for_printing.put(partial_solution)
