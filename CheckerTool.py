c6afc1ddc47814a1ace39f04360097ec7af46124
Gheorghica Radu <radugheorghica@protonmail.com>
2020-06-29 15:01:14 -0700

    Poate face verificarea rezultatelor intre GR1 si VF2 sau STwig Algorithm.

--- CheckerTool.py
+++ CheckerTool.py
@@ -1,9 +1,10 @@
 from asyncio import wait
 
-f1 = open("file_GNS1_Backtracking_STwig_Matching_Algorithm_output.txt", "r+")
+# f1 = open("file_GNS1_Backtracking_STwig_Matching_Algorithm_output.txt", "r+")
+f1 = open("file_Parallel_Backtracking_Algorithm_with_STwig_query_graphs_OUTPUT.txt", "r+")
 
-# f2 = open("file_STwig Algorithm output.txt", "r+")
-f2 = open("file_VF2 Algorithm output.txt", "r+")
+f2 = open("file_STwig Algorithm output.txt", "r+")
+# f2 = open("file_VF2 Algorithm output.txt", "r+")
 
 
 # https://www.techbeamers.com/python-read-file-line-by-line/
@@ -12,7 +13,9 @@ f1_string_lines = []
 f2_string_lines = []
 
 # PENTRU GNS1 ############## RAMANE TOT TIMPUL UNCOMMENTED #########################
-with open("file_GNS1_Backtracking_STwig_Matching_Algorithm_output.txt", "r") as rd:
+# with open("file_GNS1_Backtracking_STwig_Matching_Algorithm_output.txt", "r") as rd:
+with open("file_Parallel_Backtracking_Algorithm_with_STwig_query_graphs_OUTPUT.txt", "r") as rd:
+
 # with open("file_STwig Algorithm output.txt", "r") as rd:
     # Read lines in loop
     for line in rd:
@@ -22,19 +25,19 @@ with open("file_GNS1_Backtracking_STwig_Matching_Algorithm_output.txt", "r") as 
 
 
 # PENTRU STWIG ALGORITHM ######## DE AICI SE DECOMENTEAZA ALG CU CARE SE FACE COMPARAREA ###########
-# with open("file_STwig Algorithm output.txt", "r") as rd2:
-#     # Read lines in loop
-#     for line in rd2:
-#         # All lines (besides the last) will include  newline, so strip it
-#         f2_string_lines.append(line.strip())
-# PENTRU STWIG ALGORITHM ###########################################################
-
-# PENTRU VF2 ALGORITHM #############################################################
-with open("file_VF2 Algorithm output.txt", "r") as rd2:
+with open("file_STwig Algorithm output.txt", "r") as rd2:
     # Read lines in loop
     for line in rd2:
         # All lines (besides the last) will include  newline, so strip it
         f2_string_lines.append(line.strip())
+# PENTRU STWIG ALGORITHM ###########################################################
+
+# PENTRU VF2 ALGORITHM #############################################################
+# with open("file_VF2 Algorithm output.txt", "r") as rd2:
+#     # Read lines in loop
+#     for line in rd2:
+#         # All lines (besides the last) will include  newline, so strip it
+#         f2_string_lines.append(line.strip())
 # PENTRU VF2 ALGORITHM #############################################################
 
 # CONVERSIA DE LA STRING DIN FISIERE LA INT ########################################
@@ -52,25 +55,25 @@ for stwig_line in f1_string_lines:
 # PENTRU GNS1 ALGORITHM. RAMANE TOT TIMPUL DECOMENTAT ###############################
 
 # PENTRU STWIG ALGORITHM ############################################################
-# f2_int_lines = []
-# int_stwig_line = []
-# for stwig_line in f2_string_lines:
-#     string_stwig_line = stwig_line.split(" ")
-#     for string_stwig_line_element in string_stwig_line:
-#         int_stwig_line.append(int(string_stwig_line_element))
-#     f2_int_lines.append(int_stwig_line)
-#     int_stwig_line = []
+f2_int_lines = []
+int_stwig_line = []
+for stwig_line in f2_string_lines:
+    string_stwig_line = stwig_line.split(" ")
+    for string_stwig_line_element in string_stwig_line:
+        int_stwig_line.append(int(string_stwig_line_element))
+    f2_int_lines.append(int_stwig_line)
+    int_stwig_line = []
 # PENTRU STWIG ALGORITHM ############################################################
 
 # PENTRU VF2 ALGORITHM ##############################################################
-f2_int_lines = []
-int_vf2_line = []
-for vf2_line in f2_string_lines:
-    string_vf2_line = vf2_line.split(" ")
-    for string_vf2_line_element in string_vf2_line:
-        int_vf2_line.append(int(string_vf2_line_element))
-    f2_int_lines.append(int_vf2_line)
-    int_vf2_line = []
+# f2_int_lines = []
+# int_vf2_line = []
+# for vf2_line in f2_string_lines:
+#     string_vf2_line = vf2_line.split(" ")
+#     for string_vf2_line_element in string_vf2_line:
+#         int_vf2_line.append(int(string_vf2_line_element))
+#     f2_int_lines.append(int_vf2_line)
+#     int_vf2_line = []
 # PENTRU VF2 ALGORITHM ##############################################################
 
 # CONVERSIA DE LA STRING DIN FISIERE LA INT #########################################
@@ -130,10 +133,10 @@ for vf2_line_1 in f2_int_lines:
         # counter += 1
         result = all(elem in vf2_line_1 for elem in gns1_line1)
         if result:
-            print("GNS1 line: " + str(gns1_line1) + " | VF2 Alg line: " + str(vf2_line_1) + " | " + str(result))
+            print("Bactracking parallel line: " + str(gns1_line1) + " | VF2 Alg line: " + str(vf2_line_1) + " | " + str(result))
             counter = counter + 1
 print()
-print("counter_gns1: " + str(counter_gns1))
+print("counter_parallel_bactracking: " + str(counter_gns1))
 print("counter_vf2: " + str(counter_vf2))
 print("total count, must be equal to previous counters, who must also be equal to eachother: ")
 print(counter)
