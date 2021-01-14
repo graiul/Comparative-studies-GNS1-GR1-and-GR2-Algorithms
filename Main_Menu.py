ed09b94c6143b685583e1856ebf8f9f52802b337
Gheorghica Radu <radugheorghica@protonmail.com>
2021-01-14 14:46:29 -0800

    Optiunea 14: Am modificat numele directorului de iesire ce va contine timpi de executie, graful query si rezultatele. Numele va fi pentru GR2 Algorithm\Test 2. Am adaugat si notita legat de calculul automat al mediei aritmetice al timpilor de executie.

--- Main_Menu.py
+++ Main_Menu.py
@@ -973,10 +973,11 @@ def main():
         # Rulare cu graf query intreg, fara descompunere
         query_graph = tools.obtain_query_graph_non_stwig()
         print(query_graph)
-        a0 = GR2_Algorithm(query_graph, data_graph, False, 'C:/Users/StationG/Desktop/Baterie Teste GR2_Algorithm/Test 1/')
+        a0 = GR2_Algorithm(query_graph, data_graph, False, 'C:/Users/StationG/Desktop/Baterie Teste GR2_Algorithm/Test 2/')
         a0.execute_gr2_algorithm()
+        # Pentru calculul automat al mediei aritmetice al timpilor de executie:
         # execution_times.append(a0.get_execution_time_gr2_algorithm())
-        # create_execution_times_and_avg_txt_file_with_dir('C:/Users/StationG/Desktop/Baterie Teste GR1_Algorithm/Test 0/', execution_times)
+        # create_execution_times_and_avg_txt_file_with_dir('C:/Users/StationG/Desktop/Baterie Teste GR2_Algorithm/Test 2/', execution_times)
 ############################ Din GR1 Algorithm ##########################################################
 
 
