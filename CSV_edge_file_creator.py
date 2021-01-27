d92da6b6862d3310d9ae5b2cf77c029c3dfcc622
Gheorghica Radu <radugheorghica@protonmail.com>
2021-01-27 18:14:56 +0200

    Am schimbat un path.

--- CSV_edge_file_creator.py
+++ CSV_edge_file_creator.py
@@ -1,6 +1,6 @@
-with open('RI_data_graph_edges_for_csv.txt') as f:
+with open('DATASET RI Caenorhabditis elegans PPI/Caenorhabditis_elegans_PPI_udistr_32_edges_for_CSV.txt') as f:
     lines = f.readlines()
-f = open('RI_data_graph_edges.csv', "w+")
+f = open('DATASET RI Caenorhabditis elegans PPI/Caenorhabditis_elegans_PPI_udistr_32_edges.csv', "w+")
 f.write("from,to\n")
 for line in lines:
     new_line = line.split(" ")
