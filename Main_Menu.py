1e03748e0657e8adc09df4056a4eb5c01b56fc6e
Gheorghica Radu <radugheorghica@protonmail.com>
2021-01-30 18:33:23 +0200

    Am schimbat un path la optiunea 14 GR2 Algorithm.

--- Main_Menu.py
+++ Main_Menu.py
@@ -972,13 +972,23 @@ def main():
 
         # Rulare cu graf query intreg, fara descompunere
         query_graph = tools.obtain_query_graph_non_stwig()
-        print(query_graph)
-        a0 = GR2_Algorithm(query_graph, data_graph, False, 'C:/Users/StationG/Desktop/16 IAN 2021 Baterie Teste Finale RI Human PPI GR2_Algorithm/Test 4/')
+        # print(query_graph)
+        a0 = GR2_Algorithm(query_graph, data_graph, False, 'C:/Users/StationG/Desktop/30 IAN 2021 Baterie Teste Finale RI Bos_taurus PPI GR2_Algorithm/Test 5/A treia bucata query/')
         a0.execute_gr2_algorithm()
+
+        # Rulare cu despartirea grafului query in mai multe bucati.
+        # query_graph = tools.obtain_query_graph_stwig(wanted_parts=2)[0]  # Listele sunt stocate pe prima pozitie al unei liste mai mari, de aceea folosesc [0].
+        # print(query_graph)
+        # a1 = GR2_Algorithm(query_graph[0], data_graph, False, 'C:/Users/StationG/Desktop/20 IAN 2021 Baterie Teste Finale RI Human PPI GR2_Algorithm/Test 5/')
+        # a1.execute_gr2_algorithm()
+
         # Pentru calculul automat al mediei aritmetice al timpilor de executie:
         # Trebuie executat dupa numarul de teste dorit per graf query.
         # execution_times.append(a0.get_execution_time_gr2_algorithm())
-        # create_execution_times_and_avg_txt_file_with_dir('C:/Users/graiul/Desktop/Baterie Teste GR2_Algorithm/Test 3/', execution_times)
+        # tools.create_execution_times_and_avg_txt_file_with_dir('C:/Users/graiul/Desktop/Baterie Teste GR2_Algorithm/Test 3/', execution_times)
+        # tools.create_execution_times_and_avg_txt_file_at_dir_path()
+
+
 ############################ Din GR1 Algorithm ##########################################################
 
 
